7f6aa3eb8699bec904eb386ba10a55411b1e2d2a
wat-aro <wat-aro@users.noreply.github.com>
2022-05-07 14:55:06 +0000

    deploy: fdad56fc89d23b76ac576592f0cab7752ab52521

---
+++ _next/static/Rmz7Am1PbYLwwG2kvvHiS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cbee583a74e6198b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/about":["static/chunks/pages/about-a548f0f1148eca46.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
