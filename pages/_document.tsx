af653eb7977e30337b5f65388b4a1551257bf87e
wat-aro <kazutas1008@gmail.com>
2022-05-14 01:24:55 +0900

    GTM for development

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
-const googleTagManagerId = 'GTM-MW6D56L';
+const googleTagManagerId =
+  process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
+    : '';
 
 const GoogleTagManager: React.FC = () => {
   return (
     <script
       dangerouslySetInnerHTML={{
         __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.nsertBefore(j,f);})(window,document,'script','dataLayer','GTM-MW6D56L');
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MW6D56L');
       `,
       }}
     />
@@ -24,6 +27,17 @@ export default function Document() {
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            />`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>
