34772f2e28bb2e2264adf033e69a53b214ec86de
wat-aro <kazutas1008@gmail.com>
2022-05-14 01:01:24 +0900

    Fix GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,10 @@ const googleTagManagerId = 'GTM-MW6D56L';
 
 const GoogleTagManager: React.FC = () => {
   return (
-    <Script
-      id="gtm"
-      strategy="afterInteractive"
+    <script
       dangerouslySetInnerHTML={{
         __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.nsertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.nsertBefore(j,f);})(window,document,'script','dataLayer','GTM-MW6D56L');
       `,
       }}
     />
@@ -21,6 +19,8 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <Script src="kazutaro.js" />
+        <meta hoge="hoge" />
         <GoogleTagManager />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
