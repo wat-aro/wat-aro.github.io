3be64ab5905d6c2e2e5cf515ba2ceedc3af1d184
wat-aro <kazutas1008@gmail.com>
2022-05-14 14:20:25 +0900

    Use variable for google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
-const googleTagManagerId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+const googleTagManagerId = 'GTM-MW6D56L';
 const isProduction = process.env.NODE_ENV === 'production';
 
 const GoogleTagManager: React.FC = () => {
@@ -9,7 +9,7 @@ const GoogleTagManager: React.FC = () => {
     <script
       dangerouslySetInnerHTML={{
         __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MW6D56L');
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');
       `,
       }}
     />
@@ -28,7 +28,7 @@ export default function Document() {
           dangerouslySetInnerHTML={{
             __html: `
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-MW6D56L"
+              src="https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}"
               height="0"
               width="0"
               style="display:none;visibility:hidden"
