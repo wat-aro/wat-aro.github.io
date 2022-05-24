b876ecbbf6c822f055426e182c8cb412e69dac88
wat-aro <kazutas1008@gmail.com>
2022-05-24 12:28:54 +0900

    Fix GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,33 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import 'zenn-content-css';
+import Script from 'next/script';
+
+const googleTagManagerId = 'GTM-MW6D56L';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const GoogleTagManager: React.FC = () => {
+  return (
+    <Script
+      id="gtag-base"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${googleTagManagerId}');
+      `,
+      }}
+    />
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="py-0 md:px-8 min-h-screen flex flex-col items-center">
+      {isProduction && <GoogleTagManager />}
       <Header />
       <Component {...pageProps} />
     </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,42 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 const googleTagManagerId = 'GTM-MW6D56L';
-const isProduction = process.env.NODE_ENV === 'production';
-
-const GoogleTagManager: React.FC = () => {
-  return (
-    <Script
-      id="gtm"
-      strategy="beforeInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');
-      `,
-      }}
-    />
-  );
-};
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        {isProduction && <GoogleTagManager />}
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            />`,
-          }}
-        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
