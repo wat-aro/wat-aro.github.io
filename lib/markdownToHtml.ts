6c090089b2dfa8de994cfc50059238c9d0eb5e64
wat-aro <kazutas1008@gmail.com>
2022-06-12 19:39:18 +0900

    Show code block for hatena blog entry

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -8,6 +8,10 @@ import rehypeStringify from 'rehype-stringify';
 import rehypePrismPlus from 'rehype-prism-plus';
 import remarkBreaks from 'remark-breaks';
 import rehypeSlug from 'rehype-slug';
+import { Plugin } from 'unified';
+import { visit } from 'unist-util-visit';
+import rehypeRaw from 'rehype-raw';
+import { Element, ElementContent } from 'hast';
 
 export const markdownToHtml = async (markdown: string) => {
   const result = await unified()
@@ -16,11 +20,36 @@ export const markdownToHtml = async (markdown: string) => {
     .use(remarkBreaks)
     .use(remarkEmoji)
     .use(remarkGfm)
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(rehypeSlug)
+    .use(rehypeHatenaCodeBlock)
     .use(rehypePrismPlus, { ignoreMissing: true })
-    .use(rehypeStringify)
+    .use(rehypeStringify, { allowDangerousHtml: true })
     .process(markdown);
 
   return result.toString();
+};
+
+const rehypeHatenaCodeBlock: Plugin = () => {
+  return (tree, file) => {
+    visit(tree, 'element', (node: Element) => {
+      if (
+        node.tagName == 'pre' &&
+        node.properties?.className &&
+        Array.isArray(node.properties.className) &&
+        node.properties.className.includes('code')
+      ) {
+        const children = node.children;
+        const code = {
+          type: 'element',
+          tagName: 'code',
+          properties: {},
+          children: children,
+        } as ElementContent;
+        node.children = [code];
+      }
+      return true;
+    });
+  };
 };
