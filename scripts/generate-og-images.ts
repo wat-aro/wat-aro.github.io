8665f07771f36d8cc909d205b96c7380cea8361d
wat-aro <kazutas1008@gmail.com>
2022-06-13 21:48:18 +0900

    Use generate-ogimages properly

--- scripts/generate-og-images.ts
+++ scripts/generate-og-images.ts
@@ -1,24 +1,24 @@
+import fs from 'fs';
 import { join } from 'path';
 import { getPostByPath } from '../lib/api';
 import { getFiles } from '../lib/getFiles';
 import { takeScreenshot } from '../lib/ogimage/takeScreenShot';
 
 const generateOgImages = async () => {
-  const start = process.argv[2];
-  const end = process.argv[3];
-
   const contentsDir = join(process.cwd(), 'contents');
   const postsDir = join(contentsDir, 'posts');
   const files = await getFiles(postsDir);
-  const posts = files.slice(Number(start), Number(end)).map((file) => ({
+  const posts = files.map((file) => ({
     ...getPostByPath(file),
     slug: file.split('/').splice(-1)[0].replace(/\.md$/, ''),
   }));
 
   Promise.all(
-    posts.map((post) => {
-      takeScreenshot({ title: post.data.title, slug: post.slug });
-    })
+    posts
+      .filter((post) => !fs.existsSync(`public/og-images/${post.slug}.png`))
+      .map((post) => {
+        takeScreenshot({ title: post.data.title, slug: post.slug });
+      })
   );
 };
 
