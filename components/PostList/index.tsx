96b5384ddfd97758e764ff3f9e14c611d1d38852
wat-aro <kazutas1008@gmail.com>
2022-06-16 09:56:12 +0900

    Add PostListItem

--- components/PostList/index.tsx
+++ components/PostList/index.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
 import { Post } from '../../lib/api';
-import { postPath } from '../../lib/postPath';
+import { PostListItem } from '../PostListItem';
 
 const sortByPublishedDate = (posts: Post[]): Post[] => {
   const copied = [...posts];
@@ -23,16 +22,7 @@ export const PostList: React.FC<Props> = ({ posts }) => {
   return (
     <>
       {sorted.map((post) => (
-        <Link href={postPath(post)} key={post.slug}>
-          <a className="border-b-2 border-gray-200 border-opacity-0 hover:border-opacity-100">
-            <div className="flex pt-2 pb-1 justify-between">
-              <div className="text-xl md:w-5/6 w-9/12">{post.data.title}</div>
-              <div className="text-gray-500 md:w-1/6 w-3/12 flex justify-end">
-                {post.data.published}
-              </div>
-            </div>
-          </a>
-        </Link>
+        <PostListItem post={post} />
       ))}
     </>
   );
---
+++ components/PostListItem/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { Post } from '../../lib/api';
+import { postPath } from '../../lib/postPath';
+
+type Props = {
+  post: Post;
+};
+
+export const PostListItem: React.FC<Props> = ({ post }) => {
+  return (
+    <Link href={postPath(post)} key={post.slug}>
+      <a className="border-b-2 border-gray-200 border-opacity-0 hover:border-opacity-100">
+        <div className="flex pt-2 pb-1 justify-between">
+          <div className="text-xl md:w-5/6 w-9/12">{post.data.title}</div>
+          <div className="text-gray-500 md:w-1/6 w-3/12 flex justify-end">
+            {post.data.published}
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
