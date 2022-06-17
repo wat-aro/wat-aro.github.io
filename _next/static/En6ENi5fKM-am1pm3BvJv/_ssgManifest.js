2a1f4f908857ec0e439de3de1e1eeca5ab1502b8
wat-aro <wat-aro@users.noreply.github.com>
2022-06-17 14:47:06 +0000

    deploy: 930bdca45e7a8dbf7b897b0a38cc2fb9bf4affb8

---
+++ _next/static/En6ENi5fKM-am1pm3BvJv/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002F","\u002Fposts","\u002Fposts\u002F[slug]","\u002Fposts\u002Fpages\u002F[page]","\u002Fposts\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
