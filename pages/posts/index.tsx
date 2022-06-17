c6ad612f2cacc18cc0f5b70ea9bed1f50c09388e
wat-aro <kazutas1008@gmail.com>
2022-06-17 22:35:06 +0900

    Add /posts page

---
+++ pages/posts/index.tsx
@@ -0,0 +1,50 @@
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import { Layout } from '../../components/Layout';
+import { Pagination } from '../../components/Pagination';
+import { PostList } from '../../components/PostList';
+import { range } from '../../lib/range';
+import { sortByPublishedDate } from '../../lib/sortByPublishedDate';
+import PostRepository from '../../lib/repository/post';
+import { Post } from '../../lib/post';
+
+const perPage = 20;
+
+type Props = {
+  posts: Post[];
+  currentPage: number;
+  pages: number[];
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const currentPage = 1;
+  const posts = await PostRepository.list();
+  const sorted = sortByPublishedDate(posts);
+  const sliced = sorted.slice(
+    perPage * (currentPage - 1),
+    perPage * currentPage
+  );
+  const maxPageNumer = Math.ceil(posts.length / 20);
+  const pages = range(maxPageNumer);
+
+  return {
+    props: { posts: sliced, currentPage, pages },
+  };
+};
+
+const Posts: NextPage<Props> = ({ posts, pages, currentPage }) => {
+  return (
+    <>
+      <Head>
+        <title>Posts | (wat-aro)</title>
+      </Head>
+      <Layout>
+        <h1 className="text-3xl mb-4">Posts</h1>
+        <PostList posts={posts} />
+        <Pagination pages={pages} currentPage={currentPage} />
+      </Layout>
+    </>
+  );
+};
+
+export default Posts;
